297957d9f8874204b7d36ec32706212bf48d9661
ram-0003 <ramachandran85966@gmail.com>
2024-11-01 03:57:10 -0700

    mongodb connected

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import express from "express";
-import movieRoute from "./route/movies.route.js"
-
+import movieRoute from "./route/movies.route.js";
+import connectDB from "./lib/db.js";
 const app = express();
 const PORT=8080;
 
 app.get("/",(req,res)=>{
-    res.send("Helo Ram!!    ")
-})
+    res.send("Helo Ram!!");
+});
+
+connectDB();
 
 app.use("/movies",movieRoute);
 app.listen(PORT,()=>{
